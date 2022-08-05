8503ffa0db6cf937ab9721490b8a22992229542e
Hemanth <hkumar@myeduate.com>
2022-08-05 11:32:20 +0530

    Notes created

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Note } from "./models/note.model";
+import NotesList from "./components/NotesList";
+import Header from "./components/Header";
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import CreateNotes from "./components/CreateNotes";
+
+function App() {
+  const [notes, setnotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: "Meeting",
+      text: "Meeting is Scheduled",
+      color: "#dfdfdf",
+      date: new Date().toString(),
+    },
+  ]);
+  console.log(notes);
+  return (
+    <>
+      <Header />
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <NotesList notes={notes} setnotes={setnotes} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNotes notes={notes} setnotes={setnotes} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NotesList.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Notes from "./Notes";
+import { Note } from "../models/note.model";
+
+interface INotesListProps {
+  notes: Note[];
+  setnotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+
+const NotesList: React.FC<INotesListProps> = ({ notes, setnotes }) => {
+  const handleDelete = (id: string) => {
+    setnotes(notes.filter((note) => note.id !== id));
+  };
+  const renderNotes = (): JSX.Element[] => {
+    return notes.map((note) => {
+      return <Notes key={note.id} note={note} handleDelete={handleDelete} />;
+    });
+  };
+  return (
+    <>
+      <h2 className="mt-3">Notes </h2>
+
+      <div>{renderNotes()}</div>
+    </>
+  );
+};
+
+export default NotesList;
